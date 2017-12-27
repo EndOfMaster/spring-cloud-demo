fd4f1ec7d0ba6115b12118f7cdee7ff1833bcbc8
EndOfMaster <843698826@qq.com>
2017-12-27 17:39:49 +0800

    add eureka  Peer Awareness

--- spring-cloud-config-server/src/main/java/com/endofmaster/ConfigServer.java
+++ spring-cloud-config-server/src/main/java/com/endofmaster/ConfigServer.java
@@ -2,17 +2,21 @@ package com.endofmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ZM.Wang
  */
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServer.class, args);
+        new SpringApplicationBuilder(ConfigServer.class).web(true).run(args);
     }
 
 }
